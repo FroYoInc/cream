de5e9776f8fa43c4a0821da8c391488dbb863301
Ladinu Chandrasinghe <ladinu@gmail.com>
2015-05-19 18:38:34 -0700

    Typescript files are now recompiled upon save

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,16 @@ var ts = require('gulp-typescript');
 var debug = require('gulp-debug');
 
 var tsProject = ts.createProject('tsconfig.json');
+var tsOutDir = tsProject.config.compilerOptions.outDir;
+var tsFilesGlob = tsProject.config.filesGlob;
 
 gulp.task('transpile-ts2js', function () {
-  var dest = tsProject.options.outDir;
   var tsResults = tsProject.src().pipe(ts(tsProject));
-  return tsResults.js.pipe(gulp.dest(dest));
+  return tsResults.js.pipe(gulp.dest(tsOutDir));
 });
 
-gulp.task('default', ['transpile-ts2js']);
+gulp.task('watch-ts-files', function() {
+   gulp.watch(tsFilesGlob, ['transpile-ts2js']);
+});
+
+gulp.task('default', ['watch-ts-files', 'transpile-ts2js']);
