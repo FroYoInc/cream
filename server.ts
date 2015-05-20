a51f5d23835b105211722c5b1b1ab8499f5c4460
Ladinu Chandrasinghe <ladinu@gmail.com>
2015-05-19 19:08:39 -0700

    Added /flavors route to server

--- server.ts
+++ server.ts
@@ -1,10 +1,16 @@
-class RequestHandler {
-  url : string;
-  constructor(_url) {
-    this.url = _url;
-  }
-}
+import restify = require('restify');
 
+var server = restify.createServer({
+  name: 'emulsion',
+  version: '0.0.0'
+});
 
-var r = new RequestHandler("asd");
-var p = r.url;
+server.get('/flavors', function(req, res, next) {
+  var flavors : string[] = ['Peanut Butter', 'Cookies N Cream', 'Cake Batter']
+  res.send({'flavors': flavors});
+  next();
+});
+
+server.listen(8080, function() {
+  console.log('> %s listening on %s', server.name, server.url);
+});
