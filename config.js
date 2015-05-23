fa16177bc0fa81335e9520acfa2d275d74aeda31
Ladinu Chandrasinghe <ladinu@gmail.com>
2015-05-22 20:45:49 -0700

    Moved all the config used in gulpfile.js into config.js

--- config.js
+++ config.js
@@ -1,11 +1,15 @@
+var path = require('path');
 var package = require('./package.json');
 var tsConfig = require('./tsconfig.json');
 var tsd = require('./tsd.json');
 
 module.exports = {
-  'tsconfig': tsConfig,
   'tsConfigFile': 'tsconfig.json',
+  'tsconfig': tsConfig,
   'tsd': tsd,
   'package': package,
-  'initFile': 'server.js',
+  'tsOutDir': tsConfig.compilerOptions.outDir,
+  'tsFilesGlob': tsConfig.filesGlob,
+  'initFilePath': path.join(tsConfig.compilerOptions.outDir, 'server.js'),
+  'testFiles': path.join(tsConfig.compilerOptions.outDir, 'test/*.js')
 }
--- gulpfile.js
+++ gulpfile.js
@@ -8,35 +8,31 @@ var jasmine = require('gulp-jasmine');
 var config = require('./config.js');
 
 var tsProject    = ts.createProject(config.tsConfigFile);
-var tsOutDir     = config.tsconfig.compilerOptions.outDir;
-var tsFilesGlob  = config.tsconfig.filesGlob;
-var initFile     = config.initFile;
-var initFilePath = path.join(tsOutDir, initFile);
 var server       = null;
 
 gulp.task('clean-js', function(cb) {
-  del(tsOutDir, cb);
+  del(config.tsOutDir, cb);
 });
 
 gulp.task('transpile-ts2js', ['clean-js'], function () {
   return tsProject.src()
                   .pipe(ts(tsProject))
                   .js
-                  .pipe(gulp.dest(tsOutDir));
+                  .pipe(gulp.dest(config.tsOutDir));
 });
 
 gulp.task('restart-server', ['transpile-ts2js'], function() {
   if (server) server.kill()
-  return server = spawn('node', [initFilePath], {stdio: 'inherit'});
+  return server = spawn('node', [config.initFilePath], {stdio: 'inherit'});
 });
 
 gulp.task('unit-tests', ['transpile-ts2js'], function() {
-  return gulp.src('built/test/*.js')
+  return gulp.src(config.testFiles)
     .pipe(jasmine());
 });
 
 gulp.task('watch-ts-files', function() {
-   gulp.watch(tsFilesGlob, ['unit-tests', 'restart-server']);
+   gulp.watch(config.tsFilesGlob, ['unit-tests', 'restart-server']);
 });
 
 gulp.task('default', ['watch-ts-files']);
