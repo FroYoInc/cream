5e59d8f47ef46e08141e6e83d7a5471260c8bfa9
jushuck <jushuck@pdx.edu>
2015-06-05 23:11:34 -0700

    Adding the getters and setters for the models that we defined during the previous group meeting

--- src/models/campus.ts
+++ src/models/campus.ts
@@ -8,4 +8,28 @@ class Campus {
   private _name : string;
   private _id : string;
   private _location: GeoCode;
+
+  get name(): string {
+  	return this._name;
+  }
+
+  set name(newName: string) {
+  	this._name = newName;
+  }
+
+  get id(): string {
+  	return this._id;
+  }
+
+  set id(newId: string) {
+  	this._id = newId;
+  }
+
+  get location(): GeoCode {
+  	return this._location;
+  }
+
+  set location(newLocation: GeoCode) {
+  	this._location = newLocation;
+  }  
 }
