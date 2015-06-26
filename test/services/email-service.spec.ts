48ad896df06469cb8f88253e13cdbd8f97d7207b
Ian Aldrighetti <ianaldrighetti@outlook.com>
2015-06-25 21:59:27 -0700

    Moved the test from the integration side to unit test side as it was no longer integration.

--- test/services/email-service.spec.ts
+++ test/services/email-service.spec.ts
@@ -1,12 +1,23 @@
 import EmailService = require('../../src/services/email-service');
 import c = require('../../src/config');
+import models = require('../../src/models/models');
 
 describe('The Email Service', () => {
   var emailService = null;
   var mockTransportConfig = null;
+  var user : models.User;
 
   beforeEach(() => {
     emailService = new EmailService.EmailService();
+
+    user = {
+      id: '4e3iu9012',
+      firstName: 'Test',
+      lastName: 'User',
+      userName: 'testuser',
+      email: 'me@froyo4life.com',
+      isAccountActivated: false
+    };
   });
 
   it('should use default config options', () => {
@@ -27,5 +38,27 @@ describe('The Email Service', () => {
     emailService.transportConfig = emailConfig;
 
     expect(emailService.transportConfig).toEqual(emailConfig);
+  });
+
+  it('should send the activation email with the activation code in it', () => {
+    var activationCode: string = 'fhjsdBHJFSJKdwe239';
+	var mailData: any;
+	
+	// Override the send function, which will just save the sent message.
+	emailService.transportConfig = {
+      send: (mail, callback) => {
+        mailData = mail;
+      }
+    };
+
+    emailService.sendActivation(user, activationCode).done((value: nodemailer.SentMessageInfo) => {
+		// Good, no error.
+    }, (error) => {
+      fail(error);
+    });
+
+    expect(mailData).toBeDefined();
+    expect(mailData.message.content).toBeDefined();
+    expect(mailData.message.content.indexOf(activationCode)).not.toEqual(-1);
   });
 });
