5c36384a111dc666a5d765a292d263c9ac1e2852
Ladinu Chandrasinghe <ladinu@gmail.com>
2015-06-28 18:51:57 -0700

    Added missing code

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,20 @@
+/// <reference path="./models/User"/>
+
 import r = require('rethinkdb');
 import restify = require('restify');
 import DBUtils = require('./dbutils/migrator');
 import c  = require('./config');
-import userService = require('./services/user-service');
-import errors = require('./errors/errors');
+import EmailService = require('./services/email-service');
+import models = require('./models/models');
+import user = require('./models/user');
+import Promise = require('bluebird');
+
+import sessions = require('express-session');
+import auth = require('./services/user-auth');
+import userSer = require('./services/user-service');
+
+var RDBStore = new (require('session-rethinkdb'))(sessions);
+
 
 var migrator = new DBUtils.Migrator();
 migrator.migrate(c.Config.db);
@@ -13,18 +24,46 @@ var server = restify.createServer({
   version: '0.0.0'
 });
 
-server.use(restify.queryParser());
+server.use(sessions({
+
+  // This should Ideally be random generated on install, that way each
+  // server will have a different key
+  secret: 'thisIsOurSecretKeyDontLoseIt',
+
+  cookie:{
+    // enable this when we have https set up to secure our session cookies
+    //secure:true,
+    maxAge: 24 * 60 * 60 * 1000  // One day in milliseconds
+  },
+  store: RDBStore({
+    servers: [
+        {host: 'localhost', port: c.Config.db.port}
+    ],
+    cleanupInterval: 60000, // optional, default is 60000 (60 seconds)
+    table: 'session', // optional, default is 'session'
+    db: "froyo"
+  }),
 
 
+  // Setting resave to false prevents the session from being saved to
+  // the store when to changes have been made.
+  resave:false,
 
+  // This prevents empty sessions from being stored.
+  saveUninitialized:false
+}));
+
+// Restify body parser provides all of the info sent to the route
+// in a JSON object.
+server.use(restify.bodyParser());
+
+var routes = new (require('./routes'))(server);
 
 server.get('/flavors', function(req, res, next) {
   var flavors : string[] = ['Peanut Butter', 'Cookies N Cream', 'Cake Batter'];
   res.send({'flavors': flavors});
   next();
 });
-
-
 
 server.listen(c.Config.app.port, function() {
   console.log('> %s listening on %s', server.name, server.url);
