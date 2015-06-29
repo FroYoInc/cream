83c63ab48ea98b5701d95752c8dd9b4c2e2ca663
Ladinu Chandrasinghe <ladinu@gmail.com>
2015-06-29 14:05:16 -0700

    Added some specs to activation controller

---
+++ integration/controllers/activation.ctrl.spec.ts
@@ -0,0 +1,16 @@
+import restify = require('restify');
+import ac = require('../../src/controllers/activation.ctrl');
+
+describe('Activation controller', () => {
+  xit('should activate a user given valid activation code', (done) => {
+    done();
+  });
+
+  xit('should redirect to login page given valid activation code', (done) => {
+    done();
+  });
+
+  xit('should redirect to an error page given invalid activation code', (done) => {
+    done();
+  })
+})
