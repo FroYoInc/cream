80510e8ec92a383f9632f006210fafbb4bf31e81
joeYeager <jayeag00@gmail.com>
2015-06-29 18:40:02 -0700

    Added Swagger-UI documentation for the login and logout route. Stubbed out the swagger.json file

--- src/routes.ts
+++ src/routes.ts
@@ -3,10 +3,22 @@ import userControllers = require('./controllers/users');
 
 class routes{
 
-    constructor(app:Restify.Server){
+    constructor(server:Restify.Server){
+
+        server.post("/users/login/", userControllers.login);
+        server.get("/users/logout", userControllers.logout);
+
         
-        app.post("/users/login/", userControllers.login);
-        app.get("/users/logout", userControllers.logout);
+        
+        /*********** Documentation routes ***********/
+        
+        // /docs does not render the css correctly, so redirect to /docs/
+        server.get('/docs', function(req, res, next){
+            res.header('Location', '/docs/');
+            res.send(302);
+        });
+        server.get(/\/docs\/?.*/, Restify.serveStatic({directory: './swagger',default: 'index.html'}));
+
     }
 }
 
