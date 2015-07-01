a8f21b860c9d694ac3f4226c6bc5de1a803cd7a7
Ladinu Chandrasinghe <ladinu@gmail.com>
2015-07-01 11:22:58 -0700

    Defined input/output json for createUser controller

--- integration/controllers/create-user.ctrl.spec.ts
+++ integration/controllers/create-user.ctrl.spec.ts
@@ -1,6 +1,7 @@
 
 import Restify = require('restify');
 import Promise = require('bluebird');
+import utils = require('../utils');
 import CreateUserController = require('../../src/controllers/create-user.ctrl');
 
 function createUser(req:Restify.Request, res:Restify.Response):Promise<void> {
@@ -12,6 +13,32 @@ function createUser(req:Restify.Request, res:Restify.Response):Promise<void> {
 
 describe('CreateUserController', () => {
   xit('should create a user', (done) => {
+
+    var userName = utils.rs();
+    var email = utils.em();
+
+    var inputJSON = {
+      'firstName': 'John',
+      'lastName': 'Doe',
+      'userName': userName,
+      'email': email,
+      'password': 'somePassword'
+    }
+
+    var outputJSON = {
+      'firstName': 'John',
+      'lastName': 'Doe',
+      'userName': userName,
+      'email': email
+    }
+
+    function test(status, json) {
+      expect(status).toBe(201);
+      expect(json).toBe(outputJSON);
+    }
+
+    // var req = <Restify.Request> {body: inputJSON, query: 'asd'};
+    var res = <Restify.Response> {json: test};
     done();
   });
 });
