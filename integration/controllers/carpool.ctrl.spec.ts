d6aadd860275df710a7cc07fac314322368979f9
Ladinu Chandrasinghe <ladinu@gmail.com>
2015-07-13 11:41:07 -0700

    Started working on the controller test

--- integration/controllers/carpool.ctrl.spec.ts
+++ integration/controllers/carpool.ctrl.spec.ts
@@ -1,22 +1,84 @@
 import restify = require('restify');
 import Promise = require('bluebird');
 import utils = require('../utils');
+import campusSvc = require('../../src/services/campus.svc');
+import userSvc = require('../../src/services/user-service');
+import models = require('../../src/models/models');
 import userService = require('../../src/services/user-service');
 import CarpoolSvc = require('../../src/services/carpool.svc');
 import CarpoolCtrl = require('../../src/controllers/carpool.ctrl');
 
 
-function fail(error) {expect(error).toBeUndefined();}
-function testTrue(result) {expect(result).toBe(true);}
-function testFalse(result) {expect(result).toBe(false);}
+function createCarpool(req:restify.Request, res:restify.Response)
+:Promise<void> {
+  return new Promise<void>((resolve, reject) => {
+    function next(err) {
+      if (err) {
+        reject(err)
+      } else {
+        resolve(null)
+      }
+    };
+    CarpoolCtrl.createCarpool(req, res, next)
+  });
+}
 
 describe('Carpool controller', () => {
 
-  xit('should create a carpool', (done) => {
-    done();
-    /*var req = <restify.Request> {params: {'activate': ''}};
-    var res = <restify.Response> {send: test0, header: test1};
+  var campus:models.Campus;
+  var owner:models.User;
 
+  beforeAll((done) => {
+    // Create a user and a campus
+    Promise.resolve()
+      .then(() => {
+        var p1 = campusSvc.createCampus('PSU', {
+          address: '1825 SW Broadway, Portland, OR 97201',
+          geoCode: {lat: 45.511490, long: -122.683357}
+        });
+        var p2 = utils.getSomeUser();
+        return [p1, p2]
+      })
+      .spread((_campus:models.Campus, _user:models.User) => {
+        campus = _campus;
+        owner = _user;
+      })
+      .catch(fail)
+      .error(fail)
+      .finally(done);
+  })
+
+  it('should create a carpool', (done) => {
+    var inputJSON = {
+      'name': 'Corpool',
+      'campus': 'PSU',
+      'description': 'first carpool',
+      'owner': owner.userName
+    };
+
+    function test0 (status, outputJSON) {
+      expect(status).toBe(201);
+      expect(outputJSON.name).toBe(inputJSON.name);
+      expect(outputJSON.description).toBe(inputJSON.description);
+      //TODO: Test rest of json like owner, campus, etc..
+      var hasHref = (outputJSON.href.indexOf('/users/') > -1);
+      expect(hasHref).toEqual(true);
+    }
+
+    var req = <restify.Request> {};
+    var res = <restify.Response> {send: test0};
+    req.body = inputJSON;
+
+    createCarpool(req, res)
+      .then(() => {
+      })
+      .then(() => {
+      })
+      .catch(fail)
+      .error(fail)
+      .finally(done);
+
+/*
     getValidActivationCode()
       .then((activationCode) => {
         req.params.activate = activationCode;
--- src/controllers/carpool.ctrl.ts
+++ src/controllers/carpool.ctrl.ts
@@ -4,6 +4,21 @@ import carpoolService = require('../services/carpool.svc')
 module CarpoolController {
   export function createCarpool(
     req:restify.Request, res:restify.Response, next:restify.Next) {
+      var carpoolName:string = req.body.name;
+      var campusName:string = req.body.campus;
+      var description:string = req.body.description;
+      var owner:string = req.body.owner;
+
+      console.log(carpoolName, campusName, description, owner);
+      carpoolService.createCarpool(
+        carpoolName, campusName, description, owner)
+        .then((carpool) => {
+          res.send(201, carpool);
+        })
+        .catch((err) => {
+          res.send(500, err)
+        })
+        .finally(next);
   }
 
   export function getCarpools(
