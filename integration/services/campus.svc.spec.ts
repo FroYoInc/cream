0f8d8a14c56695fe06d897a9b59be6b5327bad19
Minh Phan <mpflamingmarshmallow@gmail.com>
2015-07-15 19:53:09 -0700

    added integration test for if campus exist then send error

--- integration/services/campus.svc.spec.ts
+++ integration/services/campus.svc.spec.ts
@@ -11,11 +11,20 @@ import utils = require('../utils');
 var db = 'froyo';
 var table = 'carpools';
 var campusNameIndex = 'name';
+var campusID: string;
 
 var TempAddress : models.Address = {
   address : '1234 Campus way',
   geoCode : {lat : 12.00, long : 12.00}
 };
+
+enum Caught {Yes};
+function _catch() {return Caught.Yes}
+function checkCaught(arg: Caught) {
+  if (arg !== Caught.Yes) {
+    fail(new Error("Expected an exception to be caught"))
+  }
+}
 
 
 function expectFalse(arg) {
@@ -26,9 +35,10 @@ function expectTrue(arg) {
   expect(arg).toBe(true);
 }
 
-function createCampus(CampusName: string, location: models.Address) {
-  return campusSrv.createCampus(CampusName, location);
-}
+function createCampus(CampusName: string, location: models.Address)
+: () => Promise<models.Carpool> {
+    return () => {return campusSrv.createCampus(CampusName, location)}
+
 
 describe('Campus Service ' , () => {
 
@@ -37,7 +47,14 @@ describe('Campus Service ' , () => {
       .then((campus) => {
         expect(campus.name).toBe('FroCampus');
         expect(campus.address).toBe(TempAddress);
-      });
+        campusID = campus.id;
+        expect(campus.id).toBeDefined();
+      })
+      //Test of campus has already been created and
+      //check for exception
+      .then(createCampus('FroCampus', TempAddress))
+      .catch(errors.CampusNameExistsException, _catch)
+      .then(checkCaught);
     });
 
 });
