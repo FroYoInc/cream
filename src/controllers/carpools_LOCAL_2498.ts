6ac09b853d3abc802d4e1963d1d777e697a2bb8c
Minh Phan <mpflamingmarshmallow@gmail.com>
2015-08-06 17:51:28 -0700

    commited a bunch of changes to delete

---
+++ src/controllers/carpools_LOCAL_2498.ts
@@ -0,0 +1,198 @@
+/// <reference path="../../typings/express-session/express-session.d.ts" />
+import Restify = require('restify');
+import auth = require('../services/user-auth');
+import models = require('../models/models');
+import Promise = require('bluebird');
+import pv = require('../validation/parameter-validator');
+import requestServ = require('../services/request-service');
+import emailServ = require('../services/email-service');
+import carpoolServ = require('../services/carpool.svc');
+import userServ = require('../services/user-service');
+import errors = require('../errors/errors');
+
+var emailSvc = new emailServ.EmailService();
+
+module carpoolControllers{
+
+    export function requestToJoin(req: Restify.Request, res:Restify.Response, next){
+        joinRequest(req)
+          .then( (status) => {
+              res.send(status);
+          });
+        next();
+    }
+
+    export function joinRequest(req: Restify.Request) : Promise<number> {
+        return new Promise<number>((resolve, reject) => {
+          var validReq = pv.verifyParams(req.params.carpoolID);
+          if(validReq){
+            auth.checkAuth(req)
+              .then( (isAuth) => {
+                if(isAuth){
+                  requestServ.createRequest(req.session["userID"], req.params.carpoolID)
+                    .then( (result) => {
+                      if(result){
+                        // Notify the members of the carpool that someone wises to join
+                        carpoolServ.getCarpoolByID(req.params.carpoolID)
+                          .then(sendRequest)
+                          .catch(Error, (err) => {resolve(500)});
+                        resolve(201);
+                      }
+                      else{
+                        resolve(500);
+                      }
+                    }).catch(errors.CarpoolRequestConflictException, (err) => {
+                      resolve(409);
+                    });
+                }
+                else{
+                  resolve(401);
+                }
+              });
+          }
+
+          else {
+            resolve(400);
+          }
+        });
+
+      function sendRequest(carpool:models.Carpool) {
+        return emailSvc.sendRequestToJoin(carpool);
+      }
+
+    }
+
+    export function approveRequest(req: Restify.Request, res:Restify.Response, next){
+      approveUserRequest(req)
+        .then( (status) => {
+            res.send(status);
+        });
+      next();
+    }
+
+    export function approveUserRequest(req:Restify.Request){
+      return new Promise<number>((resolve, reject) => {
+        var validReq = pv.verifyParams(req.params.carpoolID, req.params.userToAddID);
+        if(validReq){
+            auth.checkAuth(req)
+              .then( (isAuth) => {
+                if(isAuth){
+                  carpoolServ.getCarpoolByID(req.params.carpoolID)
+                    .then((_carpool) => {
+                      var participants = [];
+                      _carpool.participants.map((obj) => {
+                        participants.push(obj.id);
+                      });
+                      if(participants.indexOf(req.session["userID"]) >= 0){
+                        addUser(req.params.userToAddID, req.params.carpoolID, resolve)
+                      }
+                      else{
+                        resolve(403);
+                      }
+                    })
+                    .catch(errors.CarpoolNotFoundException, (err) => {
+                      resolve(404)
+                    });
+                }
+                else{
+                  resolve(401);
+                }
+              });
+        }
+        else {
+          resolve(400);
+        }
+      });
+
+      function addUser(userID, carpoolID, resolve){
+        userServ.getUserById(userID)
+          .then( (_user) => {
+            requestServ.removeRequest(userID, carpoolID)
+              .then( (result) => {
+                carpoolServ.addUserToCarpool(carpoolID, _user)
+                  .then( (_carpool) => {
+                    resolve(200);
+                  })
+                  .catch(errors.CarpoolParticipantNotFoundException, (err) => {
+                    resolve(404);
+                  })
+                  .catch(errors.CarpoolParticipantAlreadyInCarpoolExecption, (err) => {
+                    resolve(409);
+                  })
+                  .catch(errors.CarpoolNotFoundException, (err) => {
+                    resolve(404);
+                  })
+                  .catch(Error, () => {
+                    resolve(500);
+                  })
+              })
+              .catch(errors.CarpoolRequestNotFoundException, (err) => {
+                resolve(404);
+              })
+            })
+            .catch(errors.UserNotFoundException, (err) => {
+              resolve(404);
+            })
+      }
+    }
+
+    export function denyRequest(req: Restify.Request, res:Restify.Response, next){
+      denyUserRequest(req)
+        .then( (status) => {
+            res.send(status);
+        });
+      next();
+    }
+
+    export function denyUserRequest(req:Restify.Request){
+      return new Promise<number>((resolve, reject) => {
+        var validReq = pv.verifyParams(req.params.carpoolID, req.params.userToDenyID);
+
+        if(validReq){
+          auth.checkAuth(req)
+            .then( (isAuth) => {
+              if(isAuth){
+                carpoolServ.getCarpoolByID(req.params.carpoolID)
+                  .then((_carpool) => {
+                    var participants = [];
+                    _carpool.participants.map((obj) => {
+                      participants.push(obj.id);
+                    });
+                    if(participants.indexOf(req.session["userID"]) >= 0){
+                      removeUserRequest(req.params.userToDenyID, req.params.carpoolID, resolve);
+                    }
+                    else {
+                      resolve(403);
+                    }
+                  })
+                  .catch(errors.CarpoolNotFoundException, (err) => {
+                    // If the carpool does not exist, remove the request
+                    removeUserRequest(req.params.userToDenyID, req.params.carpoolID, resolve);
+                  })
+                  .catch(Error, (err) => {
+                    resolve(500);
+                  })
+              }
+              else{
+                resolve(401)
+              }
+            })
+        }
+        else {
+          resolve(400);
+        }
+      });
+
+      function removeUserRequest(userID, carpoolID, resolve){
+        requestServ.removeRequest(userID, carpoolID)
+          .then( (result) => {
+            resolve(200);
+          })
+          .catch(errors.CarpoolRequestNotFoundException, (err) => {
+            resolve(404);
+          })
+      }
+
+    }
+}
+export = carpoolControllers;
---
+++ src/controllers/users_BASE_468.ts
@@ -0,0 +1,67 @@
+/// <reference path="../../typings/express-session/express-session.d.ts" />
+import Restify = require('restify');
+import auth = require('../services/user-auth');
+import userSer = require('../services/user-service');
+import models = require('../models/models');
+import Promise = require('bluebird');
+import pv = require('../validation/parameter-validator');
+
+
+module userControllers{
+
+    /**
+     *  The controller for the user login function, verifies the params that it needs 
+     *  to function correctly, and checks that they are all defined.  Sends a 400 if they 
+     *  are not.  If the params are all defined, then it attempts to log the user in.
+     */
+    export function login(req:Restify.Request,res:Restify.Response,next){
+        var p = req.params;
+        var validReq = pv.verifyParams(p.email, p.password);
+        if(validReq){
+            auth.authenticateUser(req, p.email, p.password)
+                .then( (status) => {
+                    if(status === 200){
+                       res.send(status, {
+                           userID : req.session["userID"],
+                           firstName: req.session["firstName"],
+                           lastName: req.session["lastName"],
+                           userName: req.session["userName"],
+                           email: req.session["email"]
+                       }); 
+                    }
+                    else{
+                        res.send(status);
+                    }
+
+                }).catch(Error, (err)=> {
+                    res.send(500,{"message": err});
+                });
+        }
+        else{
+            res.send(400,{"message": "Bad Request: invalid or missing paramters"});
+        }
+        next();
+    }
+
+    /**
+     * The controller for the user logout function.  Revokes the users session and sends
+     * a 200 if the revokation is a success and a 500 otherwise.
+     */
+    export function logout(req:Restify.Request, res:Restify.Response, next){
+        auth.revokeSession(req).then( (success) => {
+            if(!success){
+                res.send(500, {"message": "Logout failed.  Unable to revoke session."});
+            }
+            else{
+                res.header('Location', '/');
+                res.send(302);
+            }
+        });
+
+        next();
+    }
+
+
+}
+
+export = userControllers;
